ed069a9e429c560cee4d06d1d0783073d2166be9
Alexander Usmanov <alex.usm@mail.ru>
2024-11-27 20:09:27 +0100

    added something

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,7 +4,8 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var str = Console.ReadLine();
+            Console.WriteLine($"Hello, World! {str}");
             Console.ReadLine();
         }
     }
